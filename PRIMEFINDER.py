4a88a07253888f5d164a0a97fa422961ef5fd471
Muliro1 <mulirokhaemba@gmail.com>
2017-04-04 10:33:53 +0300

    negative checker
    a check to ensure that the argument passed is not negative

--- PRIMEFINDER.py
+++ PRIMEFINDER.py
@@ -1,15 +1,18 @@
 def prime_finder(n):
         # this function takes in an integer n and returns a list of prime numbers from 0 to n
-        primes = []
-        for i in range(n + 1):
-                if i == 0 or i == 1:
-                        isPrime = False
-                else:
-                        isPrime = True
-                        for x in range(2, i):
-                                if i % x == 0:
-                                        isPrime = False
-                                        break
+        if n <= 0:
+                return 'Invalid Argument'
+        else:
+                primes = []
+                for i in range(n + 1):
+                        if i == 0 or i == 1:
+                                isPrime = False
+                        else:
+                                isPrime = True
+                                for x in range(2, i):
+                                        if i % x == 0:
+                                                isPrime = False
+                                                break
                 if isPrime:
                         primes.append(i)
         return primes
