0a10f5f24e458e3dbeeabd303f355a9d64929ac7
Muliro1 <mulirokhaemba@gmail.com>
2017-04-04 10:54:18 +0300

    adding testcases

--- PRIMEFINDER.py
+++ PRIMEFINDER.py
@@ -1,3 +1,16 @@
+import unittest
+from PRIMEFINDER import prime_finder
+
+class testCase(unittest.Testcase):
+        def test_returns_primes_only(self):
+                self.assertEqual(prime_finder(10), [2,3,5,7])
+        def test_returns_integerlist(self):
+                self.assertEqual(prime_finder(20), [2,3,5,7,11,13,17,19])
+        def test_if_negative_or_zero_argument(self):
+                self.assertEqual(prime_finder(0), 'Invalid Argument')
+        def test_if_arg_is_not_integer(self):
+                self.assertEqual(prime_finder('string'), 'Invalid Argument')
+
 def prime_finder(n):
         # this function takes in an integer n and returns a list of prime numbers from 0 to n
         if n <= 0 or not isinstance(n, int):
