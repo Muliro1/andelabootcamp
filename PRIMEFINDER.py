e4d2d2b66383b00fa55c1184898d7837ac3a9551
Muliro1 <mulirokhaemba@gmail.com>
2017-04-04 09:50:55 +0300

    update to omit the integers 1 and 0 from the primes list returned

--- PRIMEFINDER.py
+++ PRIMEFINDER.py
@@ -1,11 +1,15 @@
- def primeFinder(n):
-	primes = []
-	for i in range(n + 1):
-		isPrime = True
-		for x in range(2, i):
-			if i % x == 0:
-				isPrime = False
-				break
-		if isPrime:
-			primes.append(i)
-	return primes
+def primeFinder(n):
+        primes = []
+        for i in range(n + 1):
+                if i == 0 or i == 1:
+                        isPrime = False
+                else:
+                        isPrime = True
+                        for x in range(2, i):
+                                if i % x == 0:
+                                        isPrime = False
+                                        break
+                if isPrime:
+                        primes.append(i)
+        return primes
+
