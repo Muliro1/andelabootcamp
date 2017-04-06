af50e6679abe51b4f87557ae2074158e52ae529c
Muliro1 <mulirokhaemba@gmail.com>
2017-04-06 08:23:55 +0300

    Delete pokeapi.py

--- pokeapi.py
+++
@@ -1,6 +0,0 @@
-import requests
-#this code snippet makes a simple get request to pokeapi and returns the status code
-def get_status_code(): 
-    response = requests.get(http://beta.pokeapi.co/docsv2/pokeapi.co/api/v2//)
-    return response.status_code
-    
